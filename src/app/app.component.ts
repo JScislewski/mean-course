777a7547f024b8c72a513d0f75eb372f5ba21dfd
Jakub Ściślewski <jakub.scislewski@gmail.com>
2024-06-27 15:35:14 +0200

    Updated PostCreateComponent styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html',
   standalone: true,
-  imports: [FormsModule],
+  templateUrl: './post-create.component.html',
+  styleUrl: './post-create.component.css',
+  imports: [MatInputModule, FormsModule, MatCardModule, MatButtonModule],
 })
 export class PostCreateComponent {
   enteredValue = '';
