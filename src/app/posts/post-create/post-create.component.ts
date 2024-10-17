e1e1196796233f2d6375377bc0071a2b92edf381
Jakub Ściślewski <jakub.scislewski@gmail.com>
2024-10-17 18:01:15 +0200

    Added PostsService

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,24 +1,26 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatInputModule, MatError } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { Post } from '../../models/post.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post-create',
   standalone: true,
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.css',
-  imports: [MatInputModule, FormsModule, MatCardModule, MatButtonModule],
+  imports: [MatInputModule, FormsModule, MatCardModule, MatButtonModule, MatError, CommonModule],
 })
 export class PostCreateComponent {
-  enteredTitle = '';
-  enteredContent = '';
   @Output() postCreated = new EventEmitter();
 
-  onAddPost() {
-    const post: Post = { title: this.enteredTitle, content: this.enteredContent };
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = { title: form.value.title, content: form.value.content };
     this.postCreated.emit(post);
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -3,14 +3,16 @@ import { Component, Input } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { Post } from '../../models/post.model';
+import { PostsService } from '../posts.service';
 @Component({
   selector: 'app-post-list',
   standalone: true,
   imports: [MatExpansionModule, MatFormFieldModule, CommonModule],
+  providers: [PostsService],
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent {
   @Input() posts: Post[] = [];
-  panelOpenState: boolean = false;
+  constructor(public postsService: PostsService) {}
 }
