de81d3db20c48e843a4f5ff91add4d49db67c8e2
Jakub Ściślewski <jakub.scislewski@gmail.com>
2024-10-17 15:19:57 +0200

    Added missing import

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { Post } from '../../models/post.model';
 @Component({
   selector: 'app-post-list',
   standalone: true,
