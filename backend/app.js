62936965671074a68ab1256040ea48e5e9e229a7
Jakub Ściślewski <jakub.scislewski@honeywell.com>
2024-11-21 17:19:20 +0100

    Added get posts endpoint

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First");
-  next();
-});
+app.use("/api/posts", (req, res, next) => {
+  const posts = [{ id: "fadf1234123", title: "First post", content: "post content" }];
 
-app.use((req, res, next) => {
-  res.send("Hello from express!");
+  return res.status(200).json({
+    message: "Posts fetched successfully",
+    posts: posts,
+  });
 });
 
 module.exports = app;
