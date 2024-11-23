98aaa5dec067fc604db552a0f2b31ed224692513
Jakub Ściślewski <jakub.scislewski@gmail.com>
2024-11-23 18:31:35 +0100

    Update style

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Post } from '../models/post.model';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new BehaviorSubject<Post[]>(this.posts);
 
+  constructor(private http: HttpClient) {}
+
   /**
    * Retrieves a copy of the posts array.
    * @returns a new array containing the posts
    */
-  getPosts(): Post[] {
-    return [...this.posts];
+  getPosts() {
+    this.http.get<{ message: String; posts: Post[] }>('http://localhost:3000/api/posts').subscribe((postData) => {
+      this.posts = postData.posts;
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 
   /**
@@ -29,7 +35,7 @@ export class PostsService {
    * @param content - The content of the post
    */
   addPost(title: string, content: string): void {
-    const newPost: Post = { title, content };
+    const newPost: Post = { id: '1', title, content };
     this.posts.push(newPost);
     this.postsUpdated.next([...this.posts]);
   }
